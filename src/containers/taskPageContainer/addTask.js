ac7bd84a37f2287739d36af130429f22072d1b55
hubertus.putu1 <hubertus.putu1@gmail.com>
2019-08-12 12:39:11 +0700

    remove unused

--- src/containers/taskPageContainer/addTask.js
+++ src/containers/taskPageContainer/addTask.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import _ from 'lodash';
 import { connect } from 'react-redux';
 import { Grid, Fab, Button } from '@material-ui/core';
 import { withStyles } from '@material-ui/core/styles';
